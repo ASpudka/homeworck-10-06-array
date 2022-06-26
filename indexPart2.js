7bad27542d33c89c08a9009c243022ba47d9e1a3
Aleksandra <aspudka@gmail.com>
2022-06-26 16:57:41 +0300

    fin

--- indexPart2.js
+++ indexPart2.js
@@ -12,11 +12,38 @@ console.log(hasElem(array1, "hello"));
 //3. Дан массив с числами. Проверьте, есть ли в нем два одинаковых числа подряд. Если есть - вернуть true [2,2,1], а если нет - вернуть false [1,2,1].
 
 const identicNumber = (array) => {
-  return array.forEach(arr => {
-    arr === 2;
-  });
-          }
-  
+  //разобрать массив
+  //сравнить [i]!==[i+1]
+  const arr = [...array]
+  if(arr!==arr+1){
+    return false;
+  }return true;
+}
+//console.log(identicNumber(array2));
+    
 const array2 = [2, 2, 2];
 const array3 = [1, 2, 1];
-console.log(identicNumber(array2));
+//console.log(identicNumber(array2));
+
+//4. Функция, которая возвращает массив заполненный 10 случайными числами в интервале от 5 до 33. Math.random()
+
+const getRamdom = () => {
+const array = new Array;
+for (let index = 0; index < 10; index++) {
+  array.push(Math.round(Math.random()*(33-5)+5));
+  }return array;
+}
+console.log(getRamdom());
+
+
+//5. Дан массив arr. Найдите среднее арифметическое его элементов. Проверьте задачу на массиве с элементами 12, 15, 20, 25, 59, 79.
+
+const arrayNum =[12,15,20,25,59,79]
+const getAverage = (array) => {
+ const sum = array.reduce(function(accum,vol){
+    return accum+vol;
+  })
+const length = array.length;
+  return (`${sum/(length+1)}`);
+}
+console.log(getAverage(arrayNum));
